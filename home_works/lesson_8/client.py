c1a848a80e37bdd2d1c79244f201fb2de261be84
iDen <izotov.denys@gmail.com>
2021-03-12 16:51:54 +0200

    Home work #8_2

--- home_works/lesson_8/client.py
+++ home_works/lesson_8/client.py
@@ -1,20 +1,28 @@
 import socket
 from time import time
-from math import sqrt
-from itertools import count, islice
 
 
 def is_prime(n):
-    return n > 1 and all(n % i for i in islice(count(2), int(sqrt(n) - 1)))
+    a = list(range(n + 1))
+    a[1] = 0
+    lst = []
+    i = 2
+    while i <= n:
+        if a[i] != 0:
+            lst.append(a[i])
+            for j in range(i, n + 1, i):
+                a[j] = 0
+        i += 1
+    return lst
 
 
-if __name__ == '__main__':
-    prime_list = [i for i in range(2000000 + 1) if is_prime(i)]
-    prime_len = len(prime_list)
+def client():
     start = time()
+    prime_list = is_prime(2000000)
+    prime_len = len(prime_list)
     with socket.socket() as sock:
         sock.connect(('127.0.0.1', 9090))
         sock.send(str(prime_list).encode())
-    print(f'time: {time() - start}')
-    print(f'counter: {prime_len}')
+    print(f'Client time is: {time() - start}')
+    print(f'Client counter is: {prime_len}')
     print(f'======================')
